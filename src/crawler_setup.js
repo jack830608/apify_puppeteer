d5c8d8eeb1d08e81b55ffea30c75bc427ddd7cb7
jack <jack830608@gmail.com>
2019-10-24 21:56:21 +0800

    update

--- src/crawler_setup.js
+++ src/crawler_setup.js
@@ -193,7 +193,7 @@ class CrawlerSetup {
           input: this.input,
           fetch,
           minify,
-          puppeteer
+          Apify,
         };
         let output = {};
         try {
